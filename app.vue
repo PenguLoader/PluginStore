e64f5499c082fe9510ceee5c4e6a1b02201d0cd0
Sarah Engel <justsweetluna@gmail.com>
2023-04-14 17:40:45 +0200

    Added Base

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>App</h1>
+    <router-view>
+      <h2>Hello World</h2>
+    </router-view>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
+    modules : [
+        '@nuxt/image-edge',
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/device',
+        '@nuxtjs/google-fonts',
+
+],
+    image: {
+        // The screen sizes predefined by `@nuxt/image`:
+        screens: {
+            xs: 320,
+            sm: 640,
+            md: 768,
+            lg: 1024,
+            xxl: 1536,
+            xl: 1280,
+            '2xl': 1536
         },
+        domains: [
+            'user-images.githubusercontent.com',
+        ],
     },
 })
